43a286272a57a12eb92626c4db0c5ba8a4fabd38
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-07 13:04:45 +0200

    Opprettet et lite api med produktliste 💃

---
+++ src/api/products.ts
@@ -0,0 +1,38 @@
+export const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      description: "This is product 1",
+      price: 150,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      description: "This is product 2",
+      price: 100,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      description: "This is product 3",
+      price: 100,
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      description: "This is product 4",
+      price: 100,
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      description: "This is product 5",
+      price: 100,
+    },
+    {
+      id: 6,
+      name: "Product 6",
+      description: "This is product 6",
+      price: 100,
+    },
+  ];
