239a7390f2fa105d18e4ae29a8029276c8334462
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-08 11:33:28 +0200

    La til, og fjernet useState logikk i shoppingCart

--- intro/components/ShoppingCart.tsx
+++ intro/components/ShoppingCart.tsx
@@ -8,7 +8,7 @@ export default function ShoppingCart() {
     }
 
     return(
-        // Testet først section className, men er inaktiv. Fjernet den :)
+        // Testet først section className, men er inaktiv. Fjernet den
         <>
             {/* Etter mye prøving og feiling med TailWindcss, fant jeg og ChatGPt en super løsning, for å endre tekstfargen til hvit! Mission solved! Vet ikke om dette er 'best practice' og ha style={diverse css styling} - men var litt kult at det er mulig sånn og! */}
         <button type="button" onClick={handleClick} style={{ backgroundColor: "darkblue", color: "white", padding: "8px 16px", borderRadius: "0.5rem", fontWeight: "bold"}} className="font-bold px-4 py-2 rounded-lg">
