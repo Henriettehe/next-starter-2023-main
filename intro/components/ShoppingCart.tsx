658baa264313716f72f50175efe99db74a75a050
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-08 13:53:38 +0200

    Fjernet noe dummy 👍

--- intro/components/ShoppingCart.tsx
+++ intro/components/ShoppingCart.tsx
@@ -14,6 +14,9 @@ export default function ShoppingCart() {
         <button type="button" onClick={handleClick} style={{ backgroundColor: "darkblue", color: "white", padding: "8px 16px", borderRadius: "0.5rem", fontWeight: "bold"}} className="font-bold px-4 py-2 rounded-lg">
             Legg til i handlevogn
         </button>
+
         </>
-    )
+)
 }
+
+
