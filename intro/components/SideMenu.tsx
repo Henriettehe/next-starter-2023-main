ff6aa4a25d05da11b002a8f321783761239bde67
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-08 15:07:20 +0200

    onClick=toggleMenu

--- intro/components/SideMenu.tsx
+++ intro/components/SideMenu.tsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 
 export default function SideMenu({ isOpen, onClose }) {
   const toggleMenu = () => {
-    onClose(!isOpen); // Toggle om menyen synes 
+    onClose(!isOpen); // Toggle om menyen synes eller ikke 
   };
 
   return (
     <>
       <h2>Handlekurv</h2>
-      <button>Hei</button>
+      <button onClick={toggleMenu}>Hei</button>
     </>
   );
 }
