a39fd5f8010d43430dac66e0ef14c5cba194fb38
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-08 15:29:54 +0200

    Installed icons React, brukt på handlekurv 👍

--- intro/components/SideMenu.tsx
+++ intro/components/SideMenu.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FaShoppingCart, FaTimes } from 'react-icons/fa';
+// Testet litt rundt med å legge til iconer, fra React-bibliotek. Hjelp av ChatGpt for hvordan det kan gjøres :) 
 
 export default function SideMenu({ isOpen, onClose }) {
   const toggleMenu = () => {
@@ -7,8 +9,14 @@ export default function SideMenu({ isOpen, onClose }) {
 
   return (
     <>
-      <h2>Handlekurv</h2>
-      <button>Hei</button>
+    <div className="flex items-center space-x-2">
+      <FaShoppingCart size={24} color="black" />
+      <h2 className="text-lg">Handlekurv</h2>
+    </div>
+    <button className="flex items-center space-x-1">
+        <span>Fjern</span>
+        <FaTimes size={16} color="red" />
+        </button>
     </>
   );
 }
