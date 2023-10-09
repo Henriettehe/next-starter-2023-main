3d94263d039cbaa6a2eecc5cceab5aa1935ae963
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-09 19:45:30 +0200

    Lagd index.test js 😄

---
+++ src/test/index.test.js
@@ -0,0 +1,12 @@
+import { myFunction } from ".."
+
+
+it('should return 5 when called', () => {
+    const result = myFunction()
+
+    expect(result).toBe(5)
+})
+
+it('should return 6', () => {
+    
+})
